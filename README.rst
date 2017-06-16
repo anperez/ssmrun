efa3e86f519ec88b3523ea15adb4700aeabe6951
Borislav Ivanov <borogl@gmail.com>
2017-06-16 13:50:09 -0700

    cookiecutter gh:kragniz/cookiecutter-pypackage-minimal

---
+++ README.rst
@@ -0,0 +1,28 @@
+ssmrun
+======
+
+.. image:: https://img.shields.io/pypi/v/ssmrun.svg
+    :target: https://pypi.python.org/pypi/ssmrun
+    :alt: Latest PyPI version
+
+Utilities for AWS EC2 SSM
+
+Usage
+-----
+
+Installation
+------------
+
+Requirements
+^^^^^^^^^^^^
+
+Compatibility
+-------------
+
+Licence
+-------
+
+Authors
+-------
+
+`ssmrun` was written by `Fullscreen Devops <devops@fullscreen.com>`_.
---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+requirements = [
+    'Click>=6.0',
+    'boto3>=1.3.1'
+]
+
+setuptools.setup(
+    name="ssmrun",
+    version="0.1.0",
+    url="https://github.com/Fullscreen/ssmrun",
+
+    author="Fullscreen Devops",
+    author_email="devops@fullscreen.com",
+
+    description="Utilities for AWS EC2 SSM",
+    long_description=open('README.rst').read(),
+
+    packages=[
+        'ssmrun',
+    ],
+    package_dir={'ssmrun':
+                 'ssmrun'},
+    entry_points={
+        'console_scripts': [
+            'ssmrun=ssmrun.cli:main'
+        ]
+    },
+
+    install_requires=requirements,
+    license="MIT license",
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
